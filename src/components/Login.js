982ba150956c4797c4c921875f33535635f8b575
Sheena Taneja <sheena1taneja@gmail.com>
2023-10-28 20:31:33 +0530

    firebase setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,22 @@
-import React, {useState} from "react";
+import React, {useState,useRef} from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [isValidationError, setIsValidationError] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   }
+
+  const handleButtonOnClick = () => {
+    const msg = checkValidData(email.current.value, password.current.value);
+    setIsValidationError(msg);
+  }
+
   return (
     <div>
       <Header />
@@ -17,24 +27,27 @@ const Login = () => {
         />
       </div>
       <div className="flex h-screen items-center justify-center">
-        <form className="p-12 bg-black absolute w-3/12 text-white rounded-lg opacity-90">
-          <h1 className="font-bold text-3xl p-4">{isSignInForm ? "Sign In":"Sign Up"}</h1>
+        <form onSubmit={((e)=> e.preventDefault())} className="p-12 bg-black absolute w-3/12 text-white rounded-lg opacity-80">
+          <h1 className="font-bold text-3xl p-2">{isSignInForm ? "Sign In":"Sign Up"}</h1>
           {!isSignInForm && <input
             type="text"
             placeholder="Your Name"
-            className="p-4 my-4 w-full bg-gray-800 h-12 rounded-lg"
+            className="p-2 my-2 w-full bg-gray-800 h-12 rounded-lg"
           />}
           <input
+            ref={email}
             type="text"
             placeholder="Email or Phone"
-            className="p-4 my-4 w-full bg-gray-800 h-12 rounded-lg"
+            className="p-2 my-2 w-full bg-gray-800 h-12 rounded-lg"
           />
           <input
+          ref={password}
             type="password"
             placeholder="Password"
-            className="p-4 my-4 w-full bg-gray-800 h-12 rounded-lg"
+            className="p-2 my-2 w-full bg-gray-800 h-12 rounded-lg"
           />
-          <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          <p className="text-red-500 text-lg font-bold p-2">{isValidationError}</p>
+          <button className="p-2 my-4 bg-red-700 w-full rounded-lg" onClick={handleButtonOnClick}>
           {isSignInForm ? "Sign In":"Sign Up"}
           </button>
           <p className="cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now": "Already a user? Sign In" }</p>
---
+++ src/utils/validate.js
@@ -0,0 +1,8 @@
+export const checkValidData = (email, password) => {
+    const isEmailValid = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/.test(email);
+    const isPasswordValid = /^(?=.*[a-z].*[a-z])(?=.*[!"#...\d].*[!"#...\d]).{8,}$/.test(password);
+    if (!isEmailValid) return "Email is not valid";
+    if(!isPasswordValid) return "Password is not valid";
+    return null;
+}
+
